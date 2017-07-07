c507a0c2d0a565f4851445eded2c2f37ea694dbf
apple <apple@appledeMac-mini.local>
2017-07-07 14:15:18 +0800

    Initial Commit

---
+++ GetLocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetLocation
+//
+//  Created by apple on 2017/7/7.
+//  Copyright © 2017年 xjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GetLocation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GetLocation
+//
+//  Created by apple on 2017/7/7.
+//  Copyright © 2017年 xjc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
